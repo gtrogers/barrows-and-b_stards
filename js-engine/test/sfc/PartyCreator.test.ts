77e30670ae94802d2adf9e1a64fcdcdd874ebacd
Gareth <gareth@gtrogers.com>
2024-07-15 09:40:50 +0100

    Fix party creator test

--- js-engine/test/sfc/PartyCreator.test.ts
+++ js-engine/test/sfc/PartyCreator.test.ts
@@ -3,14 +3,26 @@ import { render, screen, fireEvent } from "@testing-library/vue";
 
 import PartyCreator from "../../src/sfc/PartyCreator.vue";
 
+async function setInput(label: string, val: string): Promise<void> {
+  const input = screen.getByLabelText(label);
+  await fireEvent.update(input, val);
+}
+
 describe("<Party Creator />", () => {
   test("Must assign all points to proceed", async () => {
     render(PartyCreator);
 
     expect(screen.queryByText("Go forth!")).not.toBeTruthy();
-    const input = screen.getByLabelText("brawn");
 
-    await fireEvent.update(input, "9");
+
+    await setInput("brawn", "3");
+    await setInput("brains", "3");
+    await setInput("presence", "3");
+
+    await setInput("alch", "3");
+    await setInput("discipline", "3");
+    await setInput("cunn", "3");
+    await setInput("will", "3");
 
     expect(screen.queryByText("Go forth!")).toBeTruthy();
   });
